cb6d056da9428a7b91b62ecc69e638b21f646301
adam <spencerstingley6@gmail.com>
2025-07-20 13:55:13 -0500

    removing files from bug

--- annealing/agents/modules/util.py
+++
@@ -1,8 +0,0 @@
-import os, pwd
-
-def expanduser(path):
-    user = os.getenv("SUDO_USER") or os.getenv("USER")
-    home = pwd.getpwnam(user).pw_dir
-    return path.replace("~", home, 1)
-
-
--- annealing/scenarios/cmtest/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ssh cm "cd ~/rei_agent/agents && sudo python3 ./msc.py -o positioning/Scenario_AWS-LANDSAT.json -c configs/msc_agent.yaml -s 1672630635 -d -v landsat7 -v landsat9 -v landsat4 -v landsat8 run"
-
-
